4d5d2efce3a3a24313d28c8b3c935721c096bc85
yxwsbobo <yxwsbobo@gmail.com>
2018-09-08 20:06:47 +0800

    Readme.md

--- pyTest/Readme.md
+++ pyTest/Readme.md
@@ -11,3 +11,62 @@ python
 python xxx.py
 ```
 ![脚本式](scriptprogram.PNG)  
+
+> 标识符  
+* 字母  
+* 数字(*不能作为首字母*)
+* 下划线
+* 对大小写敏感  
+* 支持中文等 
+
+> 注释  
+1. 单行注释  
+```python
+# 一行注释 
+# print("hi,someone")
+
+print("hi, boys")  
+```
+2. 多行注释  
+```python
+''' 
+第一行注释
+第二行注释
+第三行注释
+'''
+
+"""
+第一行注释
+第二行注释
+第三行注释
+"""
+```  
+> 缩进  
+
+相同缩进属于同一代码块
+```python
+if True:
+    print(1)
+    print(2)
+else:
+    print(3)
+print(4)
+```
+![运行结果](suojin.PNG)  
+
+> 多行语句  
+
+一行写完一行语句,如果语句很长,可以使用反斜杠(\)来实现多行语句.
+
+> 数字(number)类型  
+
+* int(整数):只有一种整数类型int,表示为长整型,没有Python2中的long.
+* bool(布尔):true,false.
+* float(浮点数):1.33.
+* complex(复数):1+2j.  
+
+> 字符串  
+
+
+
+
--- pyTest/Test.py
+++ pyTest/Test.py
@@ -1,5 +1,33 @@
-print("hello,world.")
-Name="wangting"
-Age=28
-print(Name)
-print(Age)
+import socket
+
+sAddress = ("127.0.0.1",8090)
+
+'''
+
+sServer = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+sServer.bind(sAddress)
+sServer.listen(5)
+
+sClient, cAddr = sServer.accept()
+
+while True:
+    cData = sClient.recv(1024)
+    sClient.send("Server send :%s" %(cData))
+
+
+'''
+
+sClient = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+cAddress = ("127.0.0.1",8091)
+sClient.bind(cAddress)
+sClient.connect(sAddress)
+
+while True:
+    sendData = raw_input("> ")
+    sClient.send("Some Info")
+    sData = sClient.recv(1024)
+
+print(sData)
+
