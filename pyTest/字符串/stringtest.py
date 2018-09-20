7c906bf65beffa558a5899101388116d07cdd5e7
yxwsbobo <yxwsbobo@gmail.com>
2018-09-20 19:31:02 +0800

    stringtest.py

--- pyTest/字符串/stringtest.py
+++ pyTest/字符串/stringtest.py
@@ -2,3 +2,24 @@ first_name = "agela"
 last_name = "juliet"
 full_name = first_name + " " + last_name
 print("hi," + full_name.title() + "!")
+
+name = "judy"
+display = "hello"+"!"
+
+print("hello" + name.title() + "i treat you")
+
+print(name.title())
+print(name.upper())
+print(name.lower())
+print(display)
+print("hello " + name + ",would you like learn some Python")
+
+print(name + 'said,"our world is beautiful"')
+
+myname = " wangting "
+print(myname.rstrip())
+print(myname.lstrip())
+print(myname.strip())
+newname = myname
+myname = "wang\nting"
+print(myname)
